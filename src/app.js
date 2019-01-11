652f4721cf276082a9dabb5d63e76498d42ed821
Brandon lent <blent1050@yahoo.com>
2019-01-10 22:01:48 -0500

    File structure layed out

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
